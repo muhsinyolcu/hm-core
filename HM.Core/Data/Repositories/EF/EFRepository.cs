58864fdccc0aaa149c79becf1b24442676564000
Muhsin Yolcu <muhsin_yolcu9234@hotmail.com>
2022-11-25 15:30:35 +0300

    EF Repository updates

--- HM.Core/Data/Repositories/EF/EFRepository.cs
+++ HM.Core/Data/Repositories/EF/EFRepository.cs
@@ -1,37 +1,219 @@
 ﻿using HM.Core.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace HM.Core.Data.Repositories.EF;
-public interface IEFRepository<TEntity>
-   where TEntity : class, IEntity, new()
+public class EFRepository<TEntity> : IEFRepository<TEntity>
+    where TEntity : class, IEntity, new()
 {
-    TEntity Get(int id);
-    Task<TEntity> GetAsync(int id);
-    IQueryable<TEntity> GetAll();
-    IQueryable<TEntity> GetAllIncluding<TProperty>(params Expression<Func<TEntity, TProperty>>[] propertySelectors);
-    List<TEntity> GetAllList(Expression<Func<TEntity, bool>> predicate);
-    List<TEntity> GetAllList();
-    Task<List<TEntity>> GetAllListAsync(Expression<Func<TEntity, bool>> predicate);
-    Task<List<TEntity>> GetAllListAsync();
+    private readonly DbSet<TEntity> _dbSet;
+    private readonly DbContext _context;
+    public EFRepository(DbContext context)
+    {
+        _dbSet = context.Set<TEntity>();
+        _context = context;
+    }
 
-    TEntity Insert(TEntity entity);
-    int InsertAndGetId(TEntity entity);
-    Task<TEntity> InsertAsync(TEntity entity);
-    Task<int> InsertAndGetIdAsync(TEntity entity);
+    #region Get Section
+    public IQueryable<TEntity> GetAll()
+    {
+        var query = _dbSet.AsQueryable();
+        return query;
+    }
 
-    TEntity Update(TEntity entity);
-    Task<TEntity> UpdateAsync(TEntity entity);
+    public IQueryable<TEntity> GetAllAsNoTracking()
+    {
+        var query = _dbSet.AsNoTracking();
+        return query;
+    }
 
-    void Delete(TEntity entity);
-    void Delete(int id);
-    void Delete(Expression<Func<TEntity, bool>> predicate);
-    Task DeleteAsync(int id);
-    Task DeleteAsync(Expression<Func<TEntity, bool>> predicate);
-    Task DeleteAsync(TEntity entity);
+    public List<TEntity> GetAllList()
+    {
+        var result = _dbSet.ToList();
+        return result.ToList();
+    }
 
-    TEntity Single(Expression<Func<TEntity, bool>> predicate);
-    Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> predicate);
+    public async Task<List<TEntity>> GetAllListAsync()
+    {
+        var result = await _dbSet.AsQueryable().ToListAsync();
+        return result;
+    }
 
-    TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate);
-    Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+    public TEntity Get(int id)
+    {
+        var entity = _dbSet.FirstOrDefault(x => x.Id == id);
+        return entity;
+    }
+
+    public async Task<TEntity> GetAsync(int id)
+    {
+        var entity = await _dbSet.FindAsync(id);
+        return entity;
+    }
+
+    public IQueryable<TEntity> GetAllIncluding<TProperty>(params Expression<Func<TEntity, TProperty>>[] propertySelectors)
+    {
+        var result = _dbSet.AsQueryable();
+
+        for (int i = 0; i < propertySelectors.Length; i++)
+            result = result.Include(propertySelectors[i]);
+
+        return result;
+    }
+
+    public List<TEntity> GetAllList(Expression<Func<TEntity, bool>> predicate)
+    {
+        var result = _dbSet.Where(predicate).ToList();
+        return result;
+    }
+
+    public async Task<List<TEntity>> GetAllListAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        var result = await _dbSet.Where(predicate).ToListAsync();
+        return result;
+    }
+    #endregion
+
+    #region Insert/Add Section
+    public TEntity Insert(TEntity entity)
+    {
+        _dbSet.Add(entity);
+        _context.SaveChanges();
+
+        return entity;
+    }
+
+    public int InsertAndGetId(TEntity entity)
+    {
+        _dbSet.Add(entity);
+        _context.SaveChanges();
+
+        return entity.Id;
+    }
+
+    public async Task<int> InsertAndGetIdAsync(TEntity entity)
+    {
+        _dbSet.Add(entity);
+        await _context.SaveChangesAsync();
+
+        return entity.Id;
+    }
+
+    public async Task<TEntity> InsertAsync(TEntity entity)
+    {
+        _dbSet.Add(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public List<TEntity> AddRange(List<TEntity> entity)
+    {
+        _dbSet.AddRange(entity);
+        _context.SaveChanges();
+
+        return entity;
+    }
+
+    public async Task<List<TEntity>> AddRangeAsync(List<TEntity> entity)
+    {
+        _dbSet.AddRange(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+    #endregion
+
+    #region First/Single Section
+    public TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate)
+    {
+        var entity = _dbSet.FirstOrDefault(predicate);
+        return entity;
+    }
+
+    public async Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        var entity = await _dbSet.FirstOrDefaultAsync(predicate);
+        return entity;
+    }
+
+    public TEntity Single(Expression<Func<TEntity, bool>> predicate)
+    {
+        return _dbSet.Single(predicate);
+    }
+
+    public async Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _dbSet.SingleAsync(predicate);
+    }
+    #endregion
+
+    #region Update Section
+    public TEntity Update(TEntity entity)
+    {
+        _context.Entry<TEntity>(entity).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return entity;
+    }
+
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        _context.Entry<TEntity>(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+    #endregion
+
+    #region Delete Section
+    public void Delete(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+        _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        var entity = _dbSet.Find(id);
+        if (entity != null)
+        {
+            Delete(entity);
+        }
+    }
+
+    public void Delete(Expression<Func<TEntity, bool>> predicate)
+    {
+        var entity = _dbSet.FirstOrDefault(predicate);
+        if (entity != null)
+        {
+            Delete(entity);
+        }
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await _dbSet.FindAsync(id);
+        if (entity != null)
+        {
+            await DeleteAsync(entity);
+        }
+    }
+
+    public async Task DeleteAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        var entity = _dbSet.FirstOrDefault(predicate);
+        if (entity != null)
+        {
+            await DeleteAsync(entity);
+        }
+    }
+
+    public async Task DeleteAsync(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+    #endregion
 }
--- HM.Core/Data/Repositories/EF/IEFRepository.cs
+++ HM.Core/Data/Repositories/EF/IEFRepository.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HM.Core.Data.Models;
+using System.Linq.Expressions;
 
-namespace HM.Core.Data.Repositories.EF
+namespace HM.Core.Data.Repositories.EF;
+public interface IEFRepository<TEntity>
+   where TEntity : class, IEntity, new()
 {
-    internal class IEFRepository
-    {
-    }
+    TEntity Get(int id);
+    Task<TEntity> GetAsync(int id);
+
+    IQueryable<TEntity> GetAll();
+    IQueryable<TEntity> GetAllIncluding<TProperty>(params Expression<Func<TEntity, TProperty>>[] propertySelectors);
+    List<TEntity> GetAllList(Expression<Func<TEntity, bool>> predicate);
+    List<TEntity> GetAllList();
+    Task<List<TEntity>> GetAllListAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<List<TEntity>> GetAllListAsync();
+    IQueryable<TEntity> GetAllAsNoTracking();
+
+    TEntity Insert(TEntity entity);
+    int InsertAndGetId(TEntity entity);
+    Task<TEntity> InsertAsync(TEntity entity);
+    Task<int> InsertAndGetIdAsync(TEntity entity);
+    List<TEntity> AddRange(List<TEntity> entity);
+    Task<List<TEntity>> AddRangeAsync(List<TEntity> entity);
+
+    TEntity Update(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+
+    void Delete(TEntity entity);
+    void Delete(int id);
+    void Delete(Expression<Func<TEntity, bool>> predicate);
+    Task DeleteAsync(int id);
+    Task DeleteAsync(Expression<Func<TEntity, bool>> predicate);
+    Task DeleteAsync(TEntity entity);
+
+    TEntity Single(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> predicate);
+
+    TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
 }
